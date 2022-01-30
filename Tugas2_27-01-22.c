954fa10cc856846d35bcc13d09cf9f0e1b34551d
TanyaNW <97944718+TanyaNW@users.noreply.github.com>
2022-01-30 13:36:55 +0700

    Update Tugas2_27-01-22.c

--- Tugas2_27-01-22.c
+++ Tugas2_27-01-22.c
@@ -1,22 +1,7 @@
 #include<stdio.h>
+#include<conio.h>
 
-char bandingkanArray(int a[], int b[], int batas)
-{
-    int i = 0;
-    for (i=0; i<batas; i++)
-    {
-        if (a[i] != b[i])
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-}
-
-char buatArray(int a[], int b[] int batas)
+char buatArray(int a[], int b[], int c[], int batas)
 {
     int i = 0;
     for (i=0; i<batas; i++)
@@ -25,40 +10,76 @@ char buatArray(int a[], int b[] int batas)
         scanf("%d", &a[i]);
         printf("Masukkan waktu eksekusi : \n");
         scanf("%d", &b[i]);
+        c[i] = b[i];
     }
 }
 
 char printArray(int a[], int batas)
 {
     int i = 0;
-    for (i=0; i<batas, i++)
+    printf("\n");
+    for (i=0; i<batas; i++)
     {
-        printf("%d", a[i])
+        printf("P%d = %d\t", i, a[i]);
     }
-    printf("\n")
+    printf("\n");
 }
 
-int
-main()
+
+void main()
 {
-    int n;
-    printf("Masukkan jumlah proses yang akan dikerjakan : \n");
-    scanf("%d", &n);
+    //inisiasi variabel
+    int i, N, sum=0, count=0, y, waktuKuantum, waktuTunggu=0, waktuSelesai=0, arrayMasuk[10], arrayEksekusi[10], arrayBantu[10];
+    printf("Masukkan jumlah proses yang akan dieksekusi : ");
+    scanf("%d", &N);
+    y = N; // y adalah jumlah proses yang masih diproses oleh sistem
 
-    int arrayMasuk[n];
-    int arrayEksekusi[n];
-    int arrayAntrian[n];
+    // Membuat array yang berisikan waktu kedatangan dan waktu eksekusi tiap proses
+    buatArray(arrayMasuk, arrayEksekusi, arrayBantu, N);
+    // Menerima waktu kuantum
+    printf("Masukkan waktu kuantum : ");
+    scanf("%d", &waktuKuantum);
 
-    buatarray(arrayMasuk, arrayksekusi, n);
-
-    int quantum;
-    printf("masukkan waktu quantum : \n");
-    scanf("%d", &quantum);
-
-    for (bandingkanArray(arrayEksekusi, arrayAntrian, n)==0)
+    printf("\n\nProses sedang dieksekusi\n");
+    printf("------------------------\n\n");
+    for(sum=0, i = 0; y!=0; ) // kondisi loop dan eksekusi proses dalam loop
     {
-        //isi prosesnya (?)
-    }
+        if(i==N-1)
+        {
+            i=0;
+        }
+        else if(arrayMasuk[i+1]<=sum)
+        {
+            i++;
+        }
+        else
+        {
+            i=0;
+        }
 
+        if(arrayBantu[i] <= waktuKuantum && arrayBantu[i] > 0)
+        {
+            sum = sum + arrayBantu[i];
+            arrayBantu[i] = 0;
+            count=1;
+        }
+        else if(arrayBantu[i] > 0)
+        {
+            arrayBantu[i] = arrayBantu[i] - waktuKuantum;
+            sum = sum + waktuKuantum;
+        }
+        if(arrayBantu[i]==0 && count==1)
+        {
+            y--; // setelah selesai dieksekusi, dihilangkan
+            printf("\n\nProses \t\t Waktu Eksekusi \t\t Waktu Selesai \t\t Waktu tunggu ");
+            printf("\nProses[%d] \t\t %d\t\t\t\t %d\t\t\t %d\n\n", i, arrayEksekusi[i], sum, sum-arrayMasuk[i]-arrayEksekusi[i]);
+            waktuTunggu = waktuTunggu+sum-arrayMasuk[i]-arrayEksekusi[i];
+            waktuSelesai = waktuSelesai+sum;
+            count =0;
+        }
+
+        printf("\nAntrian Proses\n");
+        printArray(arrayBantu, N);
+    }
     return(0);
 }
